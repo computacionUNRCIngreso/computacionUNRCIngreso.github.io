9519d730a7289c275ae0776137854edf674e5b12
Marcelo Arroyo <marcelo.arroyo@gmail.com>
2018-12-26 15:28:44 -0300

    Adding api.js

---
+++ activities/api.js
@@ -0,0 +1,23 @@
+function writeConsole(message){
+    $('#textconsole').innerHTML = message;
+}
+
+function getProgram() {
+    var prog = editor.doc.getValue();
+    return prog;
+}
+
+function getCanvasContext() {
+    return context;
+}
+
+// Canvas Mouse Position: return object {x:pos, y:pos}
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    
+    return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+    };
+}
+
